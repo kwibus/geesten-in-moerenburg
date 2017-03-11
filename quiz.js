6d2b5aa04c397413577b66a1f5c3abfae994798d
Rens Sikma <de_kwibus@live.nl>
2017-03-11 20:36:21 +0100

    add action if wrong anwser

--- quiz.js
+++ quiz.js
@@ -1,5 +1,6 @@
 
 currentquestion=0;
+
 function correct(){
     document.getElementById("mark"+currentquestion).style.display="block";
     questions=document.getElementById("questions"+currentquestion)
@@ -8,6 +9,10 @@ function correct(){
         q.disabled=true;
     }
     nextgoal();
+}
+function incorrect () {
+  alert ("fout");
+ document.getElementById("hint"+currentquestion).style.display="block";
 }
 
 function nextTab (){
