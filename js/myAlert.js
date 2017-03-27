a9724c2effac77c6d97d54ed7e56cde7d3f6bc1f
Rens Sikma <de_kwibus@live.nl>
2017-03-27 22:28:40 +0200

    fix sited now works on older <ie10

--- js/myAlert.js
+++ js/myAlert.js
@@ -6,11 +6,18 @@ function myWarning(sting){
     swal("warning",sting ,'warning');
 }
 function myConfirm (string,whenSucces,whenFail){
-
-swal({
-    title:string,
-    type:'question',
-    showCancelButton: true,
+  if (L.Browser.ielt9 || L.Browser.msPointer){
+    if (confirm(string)){
+      whenSucces();
+    }else {
+      whenFail();
+    }
+  }else {
+    swal({
+      title:string,
+      type:'question',
+      showCancelButton: true,
 
     }).then( whenSucces, whenFail);
+  }
 }
