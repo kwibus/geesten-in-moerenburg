4424899a4aed633af747eea98c8bd42269de3428
Rens Sikma <de_kwibus@live.nl>
2017-03-27 22:30:02 +0200

    make source map data use https

--- js/map.js
+++ js/map.js
@@ -118,7 +118,7 @@ function initMap() {
       document.getElementById("map").style.height=window.innerHeight + "px";
       map.invalidateSize();
     };
-    L.tileLayer('http://server.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer/tile/{z}/{y}/{x}', {
+    L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer/tile/{z}/{y}/{x}', {
         attribution:'Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ, TomTom, Intermap, iPC, USGS, FAO, NPS, NRCAN, GeoBase, Kadaster NL, Ordnance Survey, Esri Japan, METI, Esri China (Hong Kong), and the GIS User Community'
             }).addTo(map);
 
