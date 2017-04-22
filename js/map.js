e77bdcdda935646f3e6cb424c52174231f3a096f
Rens Sikma <de_kwibus@live.nl>
2017-04-22 14:34:54 +0200

    add fall-back tiles map

--- js/map.js
+++ js/map.js
@@ -190,7 +190,7 @@ function initMap() {
       document.getElementById("map").style.height=window.innerHeight + "px";
       map.invalidateSize();
     };
-    var layer = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer/tile/{z}/{y}/{x}', {
+    var layer = L.tileLayer.fallback('https://server.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer/tile/{z}/{y}/{x}', {
         attribution:'Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ, TomTom, Intermap, iPC, USGS, FAO, NPS, NRCAN, GeoBase, Kadaster NL, Ordnance Survey, Esri Japan, METI, Esri China (Hong Kong), and the GIS User Community',
         useCache: true,
         // useOnlyCache: true,
